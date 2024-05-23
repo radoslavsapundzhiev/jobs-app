b1ba4dc81abcf89559856ac5a73b82fe8ad88e8c
radoslavsapundzhiev <radoslavsapundzhiev@gmail.com>
2024-05-23 16:43:02 +0300

    finished create job form

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <div class="card container ">
+    <div class="card add-form container">
         <div class="card-header">
             <p class="card-header-title">Post a job offer</p>
         </div>
@@ -7,27 +7,96 @@
             <form action="" class="form">
                 @csrf
                 <div class="field">
-                    <label class="label">Company name</label>
+                    <label class="label">Company</label>
                     <div class="control">
                         <input class="input" type="text" name="company" value="{{ old('company') }}">
                     </div>
+
+                    @error('company')
+                        <p class="has-text-danger">{{ $message }}</p>    
+                    @enderror
                 </div>
                 <div class="field">
-                    <label class="label">Name</label>
+                    <label class="label">Title</label>
                     <div class="control">
-                        <input class="input" type="text" placeholder="Text input">
+                        <input class="input" type="text" name="title" value="{{ old('title') }}">
                     </div>
+
+                    @error('title')
+                        <p class="has-text-danger">{{ $message }}</p>    
+                    @enderror
                 </div>
                 <div class="field">
-                    <label class="label">Name</label>
+                    <label class="label">Location</label>
                     <div class="control">
-                        <input class="input" type="text" placeholder="Text input">
+                        <input class="input" type="text" name="location" value="{{ old('location') }}">
                     </div>
+
+                    @error('location')
+                        <p class="has-text-danger">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="field">
-                    <label class="label">Name</label>
+                    <label class="label">Email</label>
                     <div class="control">
-                        <input class="input" type="text" placeholder="Text input">
+                        <input class="input" type="text" name="email" value="{{ old('email') }}">
+                    </div>
+
+                    @error('email')
+                        <p class="has-text-danger">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="field">
+                    <label class="label">Website</label>
+                    <div class="control">
+                        <input class="input" type="text" name="website" value="{{ old('website') }}">
+                    </div>
+
+                    @error('website')
+                        <p class="has-text-danger">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="field">
+                    <label class="label">Tags (Comma separated)</label>
+                    <div class="control">
+                        <input class="input" type="text" name="tags" value="{{ old('tags') }}">
+                    </div>
+
+                    @error('tags')
+                        <p class="has-text-danger">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="field">
+                    <label class="label">Logo</label>
+                    <div class="control">
+                        <input class="input" type="file" name="logo" value="{{ old('logo') }}">
+                    </div>
+
+                    @error('logo')
+                        <p class="has-text-danger">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="field">
+                    <label class="label">Description</label>
+                    <div class="control">
+                        <textarea
+                            class="textarea"
+                            placeholder="Job description"
+                            rows="5"
+                            name="description"
+                        >{{ old('description') }}</textarea>
+                    </div>
+
+                    @error('description')
+                        <p class="has-text-danger">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="field is-grouped">
+                    <div class="control">
+                        <button class="button is-link">Create</button>
+                    </div>
+                    <div class="control">
+                        <a href="/" class="button is-link is-light">Back</a>
                     </div>
                 </div>
             </form>
